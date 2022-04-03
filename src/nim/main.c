d7b2a0f00a6a75762212c544356c38c0298543fd
Danil Kotul <kotul20021@mail.ru>
2022-04-03 15:00:45 +0700

    add main.c

--- src/nim/main.c
+++ src/nim/main.c
@@ -2,6 +2,22 @@
 
 int main() {
 	printf("Игра 'Ним' (три кучки)\n");
+	printf("\n*****\n");
+	printf("\nТри кучки:\n");
+	printf("%-10d %-10d %-10d\n", 10, 15, 11);
+
+	int n, count;
+	printf("Введите номер кучки и количество спичек:\n");
+	scanf("%d %d", &n, &count);
+	printf("Вы ввели: %d %d\n", n, count);
+
+	printf("\n*****\n");
+	printf("\nТри кучки:\n");
+	printf("%-10d %-10d %-10d\n", 10, 15, 11);
+
+	printf("Введите номер кучки и количество спичек:\n");
+	scanf("%d %d", &n, &count);
+	printf("Вы ввели: %d %d\n", n, count);
 	return 0;
 }
 
