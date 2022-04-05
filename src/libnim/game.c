1919ffd7a6508abe9d31a344385549a0cd8a74e5
Danil Kotul <kotul20021@mail.ru>
2022-04-05 17:00:17 +0700

    add random

--- src/libnim/game.c
+++ src/libnim/game.c
@@ -1,12 +1,14 @@
 #include <libnim/game.h>
 #include <libnim/gameFuncs.h>
 
+#include <stdlib.h>
+
 void game()
 {
     int array[3];
-    array[0] = 10;
-    array[1] = 15;
-    array[2] = 11;
+    array[0] = rand() % 10 + 10;
+    array[1] = rand() % 10 + 10;
+    array[2] = rand() % 10 + 10;
 
     while (array[0] || array[1] || array[2])
         step(array);
